d6651b6e6760ef60d20a42e0179090fa33dfb9e5
Yves Candau <ycandau@sfu.ca>
2020-10-08 16:21:31 -0700

    Add DrawSprial custom block.

--- custom.ts
+++ custom.ts
@@ -1,38 +1,67 @@
-
 /**
- * Use this file to define custom functions and blocks.
- * Read more at /blocks/custom
+ * Custom blocks for the BioWearable workshop.
  */
 
-enum MyEnum {
-    //% block="one"
-    One,
-    //% block="two"
-    Two
+enum RotationDir {
+  //% block="Clockwise"
+  Clockwise = 1,
+  //% block="Counter-clockwise"
+  CounterClockwise = -1
 }
 
 /**
- * Custom blocks
- */
-//% weight=100 color=#0fbc11 icon=""
-namespace custom {
-    /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
-     */
-    //% block
-    export function foo(n: number, s: string, e: MyEnum): void {
-        // Add code here
-    }
+* Custom blocks
+*/
 
-    /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
-     */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
-    }
+//% weight=110
+//% color=#F7931E
+//% icon="\uf1b3"
+//% groups=['Input', 'Mappings', 'Output']
+namespace BioWearables {
+
+  //% block="Get simulated breath: duration=%duration"
+  //% duration.min=5 duration.defl=5
+  //% group="Input"
+  export function simulateBreath(duration: number) {
+      return (0.5 * (1 - Math.cos(Math.PI * input.runningTime() * 0.002 / duration)))
+  }
+
+  /**
+   * Draw a spiral on a Neopixel LED strip
+   * @param length The length of the spiral.
+   * @param color The color of the spiral.
+   * @param direction The direction of the spiral.
+   * @param strip The Neopixel LED strip.
+   */
+  //% block="$neoStrip|Draw spiral: length = $length|color = $color=neopixel_colors|direction = $direction"
+  //% strip.defl=strip
+  //% length.min=0 length.max=1 length.defl=0.4
+  //% inlineInputMode=inline
+  // expandableArgumentMode="toggle"
+  //% group="Output"
+  export function drawSpiral(length: number, color: number, direction: RotationDir, neoStrip: neopixel.Strip) {
+      const dx = (direction === RotationDir.Clockwise)
+          ? [1, 8, -1, -8]
+          : [8, 1, -8, -1]
+      let x = 27
+      let sideIndex = 0
+      let sideLength = 1
+      let count = 0
+
+      // Clamp to 32 bit and truncate to integer
+      length = (65 * Math.min(length, 1)) >> 0
+      neoStrip.clear()
+
+      while (count < length) {
+          sideLength = Math.min(sideLength, length - count)
+          for (let i = 0; i < sideLength; i++) {
+              neoStrip.setPixelColor(x, color)
+              x += dx[sideIndex % 4]
+          }
+          count += sideLength
+          sideLength += sideIndex % 2
+          sideIndex += 1
+      }
+      neoStrip.show()
+  }
 }
--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+let leftMotor = bBoard_Motor.createMotor(BoardID.zero, ClickID.Zero, bBoard_Motor.motorDriver.right)
+let neoStrip = neopixel.createStrip(
+BoardID.zero,
+ClickID.Zero,
+neoPin.P0,
+64,
+NeoPixelMode.RGBW
+)
+neoStrip.setBrightness(5)
 basic.forever(function () {
-	
+    BioWearables.drawSpiral(BioWearables.simulateBreath(5), neopixel.colors(NeoPixelColors.Indigo), RotationDir.CounterClockwise, neoStrip)
+    leftMotor.motorEnable(bBoard_Motor.motorState.enabled)
+    leftMotor.motorDutyDirection(50, bBoard_Motor.motorDirection.forward)
 })
