3dac5f1a3e1b9af1008708961f6b777eab05e710
Yves Candau <ycandau@sfu.ca>
2020-11-19 12:35:52 -0800

    Update

--- main.ts
+++ main.ts
@@ -1,10 +1,1 @@
-let neoStrip = neopixel.createStrip(
-BoardID.zero,
-ClickID.Zero,
-neoPin.P0,
-64,
-NeoPixelMode.RGBW
-)
-basic.forever(function () {
-    biowearables.drawDiskMicrobit(100, 100)
-})
+
